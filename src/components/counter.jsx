c37b4886fa50be099bd12ee34edca539bb12e2c2
Tran Van Duy <duytv@ait.com>
2022-04-12 17:01:47 +0700

    push src

---
+++ src/components/counter.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  styles = {
+    fontWeight: "bold",
+    fontSize: 10,
+  };
+  render() {
+    return (
+      <div>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
